2e55eff859e1e8e91685f5811450133fb09e3048
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-24 23:04:06 +0530

    added skills

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,16 @@ import { Hero } from "./components/Hero";
 import { About } from "./components/About";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import { Skills } from "./components/Skills";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-	return (
-		<main className=" bg-neutral-900 relative  w-full   ">
-			<Hero />
-			<About />
-		</main>
-	);
+  return (
+    <main className=" bg-neutral-900     ">
+      <Hero />
+      <About />
+      <Skills />
+    </main>
+  );
 }
