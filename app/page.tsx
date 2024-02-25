34097539daef6a94746e3fb88449a4963e2171d5
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-25 23:00:03 +0530

    added new stcky logic

--- app/page.tsx
+++ app/page.tsx
@@ -9,21 +9,45 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Container } from "./components/Container";
+import { useEffect, useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+  const containerRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  useEffect(() => {
+    containerRefs.current.forEach((el) => {
+      gsap.fromTo(
+        el,
+        {
+          scale: 1.1,
+        },
+        {
+          scale: 1,
+          scrollTrigger: {
+            trigger: el,
+            pin: el,
+            start: "top top",
+            end: "center 30%",
+            scrub: 1,
+            markers: true,
+          },
+        },
+      );
+    });
+  }, []);
+
   return (
-    <main className=" bg-neutral-900  ">
-      <Container>
-        <Hero />
-      </Container>
-      <Container>
-        <About />
-      </Container>
-      <Container>
-        <Skills />
-      </Container>
+    <main className=" bg-neutral-900">
+      {Array.from({ length: 5 }).map((_, i) => (
+        <div
+          key={i}
+          className="h-screen sticky top-0 w-full flex justify-center items-center"
+        >
+          <div className="h-[80%] w-[80%] bg-teal-500">HRLo</div>
+        </div>
+      ))}
     </main>
   );
 }
