621f9784cac9051a8584643b9c074944d2d93539
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-26 19:47:18 +0530

    added overlay to skill

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -4,44 +4,60 @@ import { useLayoutEffect, useEffect, useRef } from "react";
 import { data } from "../lib/data";
 
 export const Skills = () => {
-  const mainContainerRef = useRef<HTMLDivElement>(null);
-  const skillRef = useRef<(HTMLDivElement | null)[]>([]);
+	const mainContainerRef = useRef<HTMLDivElement>(null);
+	const skillRef = useRef<(HTMLDivElement | null)[]>([]);
+	const overlayRef = useRef<(HTMLDivElement | null)[]>([]);
 
-  useEffect(() => {
-    gsap.set(skillRef.current, { yPercent: 200 });
-    gsap.to(skillRef.current, {
-      yPercent: 0,
-      stagger: { each: 0.05, from: "center" },
-      scrollTrigger: {
-        trigger: mainContainerRef.current,
-        start: "top bottom",
-        end: "center center",
-        scrub: 1,
-      },
-    });
-  }, []);
+	useEffect(() => {
+		gsap.set(skillRef.current, { yPercent: 200 });
+		const tl = gsap.timeline();
+		tl.to(skillRef.current, {
+			yPercent: 0,
+			stagger: { each: 0.05, from: "center" },
+			scrollTrigger: {
+				trigger: mainContainerRef.current,
+				start: "top bottom",
+				end: "center center",
+				scrub: 1,
+			},
+		}).to(overlayRef.current, {
+			yPercent: -100,
+			stagger: { each: 0.05, from: "center" },
+			scrollTrigger: {
+				trigger: mainContainerRef.current,
+				start: "top 10%",
+				end: "top top",
+				scrub: 1,
+				markers: true,
+			},
+		});
+	}, []);
 
-  return (
-    <div
-      className="h-full rounded-3xl w-full p-10 bg-neutral-400 flex flex-col gap-10"
-      ref={mainContainerRef}
-    >
-      <h1 className="text-6xl font-poppins font-semibold">SKILLS</h1>
-      <div className="flex gap-2 h-full w-full">
-        {data.skills.data.map((skill, i) => (
-          <div
-            ref={(el) => (skillRef.current[i] = el)}
-            key={i}
-            className="bg-white h-full w-full flex relative items-end"
-          >
-            <div className="absolute bottom-0  transform left-[15%] ">
-              <h1 className="whitespace-nowrap transform text-5xl origin-top-left -rotate-90">
-                {skill.name}
-              </h1>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="h-full rounded-3xl w-full p-10 bg-neutral-400 flex flex-col gap-10"
+			ref={mainContainerRef}
+		>
+			<h1 className="text-6xl font-poppins font-semibold">SKILLS</h1>
+			<div className="flex gap-2 h-full w-full">
+				{data.skills.data.map((skill, i) => (
+					<div
+						ref={(el) => (skillRef.current[i] = el)}
+						key={i}
+						className="bg-white overflow-hidden h-full w-full flex relative items-end "
+					>
+						<div
+							ref={(el) => (overlayRef.current[i] = el)}
+							className="h-full w-full z-10 absolute mix-blend-difference bg-white origin-top"
+						></div>
+						<div className="absolute bottom-0  transform left-[15%] ">
+							<h1 className="whitespace-nowrap transform text-5xl origin-top-left -rotate-90">
+								{skill.name}
+							</h1>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
