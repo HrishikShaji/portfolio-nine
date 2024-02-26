db3cfc9cb24036df97f5e53dab137945431a3de5
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-26 23:34:42 +0530

    skillcontainer added

--- app/components/Skill.tsx
+++ app/components/Skill.tsx
@@ -3,35 +3,44 @@ import gsap from "gsap";
 import { useLayoutEffect, useEffect, useRef } from "react";
 
 interface SkillProps {
-	item: Record<string, any>;
-	active: boolean;
+  item: Record<string, any>;
+  active: boolean;
 }
 
 export const Skill: React.FC<SkillProps> = ({ item, active }) => {
-	const containerRef = useRef<HTMLDivElement>(null);
-	const headingRef = useRef<HTMLHeadingElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const headingRef = useRef<HTMLHeadingElement>(null);
+  const skillContainer = useRef<HTMLDivElement>(null);
 
-	useEffect(() => {
-		let ctx = gsap.context(() => {
-			if (active) {
-				gsap.to(headingRef.current, { opacity: 0, duration: 0.5 });
-			} else {
-				gsap.to(headingRef.current, { opacity: 1, duration: 0.5 });
-			}
-		}, containerRef);
-	}, [active]);
+  useEffect(() => {
+    let ctx = gsap.context(() => {
+      if (active) {
+        gsap.to(headingRef.current, { opacity: 0, duration: 0.5 });
+        gsap.to(skillContainer.current, { display: "block" });
+      } else {
+        gsap.to(headingRef.current, { opacity: 1, duration: 0.5 });
+        gsap.to(skillContainer.current, { display: "none" });
+      }
+    }, containerRef);
+  }, [active]);
 
-	return (
-		<div
-			ref={containerRef}
-			className="absolute bottom-0  transform left-[15%] "
-		>
-			<h1
-				ref={headingRef}
-				className="heading whitespace-nowrap transform text-5xl origin-top-left -rotate-90"
-			>
-				{item.name}
-			</h1>
-		</div>
-	);
+  return (
+    <>
+      <div
+        className="h-full w-full bg-green-500 hidden"
+        ref={skillContainer}
+      ></div>
+      <div
+        ref={containerRef}
+        className="absolute bottom-0  transform left-[15%] "
+      >
+        <h1
+          ref={headingRef}
+          className="heading whitespace-nowrap transform text-5xl origin-top-left -rotate-90"
+        >
+          {item.name}
+        </h1>
+      </div>
+    </>
+  );
 };
