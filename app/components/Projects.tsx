43b4ac3e4df34dd69158a592068920af52d75fd8
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-27 22:41:59 +0530

    added xPercent animation

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -3,52 +3,45 @@ import { useLayoutEffect, useEffect, useRef } from "react";
 import { data } from "../lib/data";
 
 export const Projects = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const mainContainerRef = useRef<HTMLDivElement>(null);
+	const containerRef = useRef<HTMLDivElement>(null);
+	const mainContainerRef = useRef<HTMLDivElement>(null);
+	const projectRefs = useRef<(HTMLDivElement | null)[]>([]);
 
-  useEffect(() => {
-    let ctx = gsap.context(() => {
-      gsap.fromTo(
-        containerRef.current,
-        {
-          rotateX: "-45deg",
-          y: 200,
-          scale: 0.95,
-        },
-        {
-          y: 0,
-          scale: 1,
-          rotateX: "0deg",
-          scrollTrigger: {
-            trigger: mainContainerRef.current,
-            start: "top center",
-            end: "center center",
-            scrub: true,
-          },
-        },
-      );
-    }, mainContainerRef);
-    return () => ctx.revert();
-  }, []);
+	useEffect(() => {
+		let ctx = gsap.context(() => {
+			const tl = gsap.timeline();
+			gsap.set(projectRefs.current, { xPercent: 100 });
+			gsap.to(projectRefs.current, {
+				xPercent: 0,
+				stagger: 0.05,
+				scrollTrigger: {
+					trigger: mainContainerRef.current,
+					start: "top bottom",
+					end: "top top",
+					scrub: true,
+					markers: true,
+				},
+			});
+		}, mainContainerRef);
+	}, []);
 
-  return (
-    <div
-      ref={mainContainerRef}
-      className="h-full rounded-3xl w-full p-10 bg-neutral-400 flex flex-col gap-10"
-    >
-      <h1 className="text-6xl font-poppins font-semibold">PROJECTS</h1>
-      <div className="flex items-center gap-100 h-full w-full">
-        <div className="flex flex-col justify-between gap-2 h-full w-full">
-          {data.projects.data.slice(0, 10).map((project, i) => (
-            <div
-              className="p-2 rounded-xl bg-white flex flex-grow h-full"
-              key={i}
-            >
-              <h1>{project.title}</h1>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			ref={mainContainerRef}
+			className="h-full rounded-3xl w-full p-10 bg-neutral-400 flex flex-col gap-10"
+		>
+			<h1 className="text-6xl font-poppins font-semibold">PROJECTS</h1>
+			<div className="flex flex-col justify-between gap-2 h-full w-full overflow-hidden">
+				{data.projects.data.slice(0, 10).map((project, i) => (
+					<div
+						ref={(el) => (projectRefs.current[i] = el)}
+						className="p-2 rounded-xl bg-white flex w-full h-full"
+						key={i}
+					>
+						<h1>{project.title}</h1>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -5,77 +5,76 @@ import { data } from "../lib/data";
 import { Skill } from "./Skill";
 
 export const Skills = () => {
-  const mainContainerRef = useRef<HTMLDivElement>(null);
-  const skillRef = useRef<(HTMLDivElement | null)[]>([]);
-  const overlayRef = useRef<(HTMLDivElement | null)[]>([]);
-  const [active, setActive] = useState(
-    Array(data.skills.data.length).fill(false),
-  );
+	const mainContainerRef = useRef<HTMLDivElement>(null);
+	const skillRef = useRef<(HTMLDivElement | null)[]>([]);
+	const overlayRef = useRef<(HTMLDivElement | null)[]>([]);
+	const [active, setActive] = useState(
+		Array(data.skills.data.length).fill(false),
+	);
 
-  useEffect(() => {
-    let ctx = gsap.context(() => {
-      const tl = gsap.timeline();
-      tl.set(skillRef.current, {
-        width: (i) => (i === 0 ? "200px" : "60px"),
-      })
-        .set(skillRef.current, { yPercent: 200 })
-        .to(skillRef.current, {
-          yPercent: 0,
-          stagger: { each: 0.05 },
-          scrollTrigger: {
-            trigger: mainContainerRef.current,
-            start: "top bottom",
-            end: "center center",
-            scrub: 1,
-          },
-        })
-        .to(overlayRef.current, {
-          yPercent: -100,
-          stagger: { each: 0.05 },
-          scrollTrigger: {
-            trigger: mainContainerRef.current,
-            start: "top 10%",
-            end: "top top",
-            scrub: 1,
-            markers: true,
-          },
-        });
-      skillRef.current.forEach((el) => {
-        el?.addEventListener("mouseenter", (e: MouseEvent) => {
-          gsap.to(skillRef.current, {
-            width: (i) => (skillRef.current[i] === e.target ? "200px" : "60px"),
-            duration: 0.3,
-            ease: "none",
-          });
-        });
-      });
-    }, mainContainerRef);
+	useEffect(() => {
+		let ctx = gsap.context(() => {
+			const tl = gsap.timeline();
+			tl.set(skillRef.current, {
+				width: (i) => (i === 0 ? "200px" : "60px"),
+			})
+				.set(skillRef.current, { yPercent: 200 })
+				.to(skillRef.current, {
+					yPercent: 0,
+					stagger: { each: 0.05 },
+					scrollTrigger: {
+						trigger: mainContainerRef.current,
+						start: "top bottom",
+						end: "center center",
+						scrub: 1,
+					},
+				})
+				.to(overlayRef.current, {
+					yPercent: -100,
+					stagger: { each: 0.05 },
+					scrollTrigger: {
+						trigger: mainContainerRef.current,
+						start: "top 10%",
+						end: "top top",
+						scrub: 1,
+					},
+				});
+			skillRef.current.forEach((el) => {
+				el?.addEventListener("mouseenter", (e: MouseEvent) => {
+					gsap.to(skillRef.current, {
+						width: (i) => (skillRef.current[i] === e.target ? "200px" : "60px"),
+						duration: 0.3,
+						ease: "none",
+					});
+				});
+			});
+		}, mainContainerRef);
 
-    return () => ctx.kill();
-  }, []);
+		return () => ctx.kill();
+	}, []);
 
-  return (
-    <div
-      className="h-full rounded-3xl w-full p-10 bg-neutral-400 flex flex-col gap-10"
-      ref={mainContainerRef}
-    >
-      <h1 className="text-6xl font-poppins font-semibold">SKILLS</h1>
-      <div className="flex h-full w-full justify-between">
-        {data.skills.data.map((skill, i) => (
-          <div
-            onMouseEnter={() => setActive((prev) => ({ ...prev, [i]: true }))}
-            onMouseLeave={() => setActive((prev) => ({ ...prev, [i]: false }))}
-            ref={(el) => (skillRef.current[i] = el)}
-            key={i}
-            className="rounded-xl bg-white overflow-hidden h-full  flex relative items-end "
-          >
-            <div
-              ref={(el) => (overlayRef.current[i] = el)}
-              className="rounded-xl h-full w-full z-10 absolute  bg-black origin-top"
-            ></div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="h-full rounded-3xl w-full p-10 bg-neutral-400 flex flex-col gap-10"
+			ref={mainContainerRef}
+		>
+			<h1 className="text-6xl font-poppins font-semibold">SKILLS</h1>
+			<div className="flex h-full w-full justify-between">
+				{data.skills.data.map((skill, i) => (
+					<div
+						onMouseEnter={() => setActive((prev) => ({ ...prev, [i]: true }))}
+						onMouseLeave={() => setActive((prev) => ({ ...prev, [i]: false }))}
+						ref={(el) => (skillRef.current[i] = el)}
+						key={i}
+						className="rounded-xl bg-white overflow-hidden h-full  flex relative items-end "
+					>
+						<div
+							ref={(el) => (overlayRef.current[i] = el)}
+							className="rounded-xl h-full w-full z-10 absolute  bg-black origin-top"
+						></div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
