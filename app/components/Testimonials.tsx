075f432bcdfbc0505a7cfda1e6c39796d5c3243d
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-28 17:15:44 +0530

    animation added testimonial

--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -3,56 +3,72 @@ import { useLayoutEffect, useEffect, useRef } from "react";
 import { data } from "../lib/data";
 
 export const Testimonials = () => {
-  const containerRef = useRef<HTMLDivElement>(null);
-  const mainContainerRef = useRef<HTMLDivElement>(null);
-  const projectRefs = useRef<(HTMLDivElement | null)[]>([]);
-  const overlayRefs = useRef<(HTMLDivElement | null)[]>([]);
+	const containerRef = useRef<HTMLDivElement>(null);
+	const mainContainerRef = useRef<HTMLDivElement>(null);
+	const testimonialRefs = useRef<(HTMLDivElement | null)[]>([]);
+	const overlayRefs = useRef<(HTMLDivElement | null)[]>([]);
 
-  useEffect(() => {
-    let ctx = gsap.context(() => {
-      const tl = gsap.timeline();
-      gsap.set(projectRefs.current, { xPercent: 100 });
-      gsap.set(overlayRefs.current, { xPercent: 0 });
-      tl.to(projectRefs.current, {
-        xPercent: 0,
-        stagger: 0.05,
-        scrollTrigger: {
-          trigger: mainContainerRef.current,
-          start: "top bottom",
-          end: "top top",
-          scrub: true,
-          markers: true,
-        },
-      }).to(overlayRefs.current, {
-        xPercent: -100,
-        stagger: 0.05,
-        scrollTrigger: {
-          trigger: mainContainerRef.current,
-          start: "top 40%",
-          end: "top 10%",
-          scrub: true,
-          markers: true,
-        },
-      });
-    }, mainContainerRef);
-  }, []);
+	useEffect(() => {
+		const coordinates = [
+			{ xPercent: 100, yPercent: 100 },
+			{ xPercent: 0, yPercent: 100 },
+			{ xPercent: -100, yPercent: 100 },
+			{ xPercent: 100, yPercent: 0 },
+			{ xPercent: 0, yPercent: 0 },
+			{ xPercent: -100, yPercent: 0 },
+			{ xPercent: 100, yPercent: -100 },
+			{ xPercent: 0, yPercent: -100 },
+			{ xPercent: -100, yPercent: -100 },
+		];
+		let ctx = gsap.context(() => {
+			const tl = gsap.timeline();
+			gsap.set(testimonialRefs.current, {
+				xPercent: (i) => coordinates[i].xPercent,
+				yPercent: (i) => coordinates[i].yPercent,
+			});
+			gsap.set(overlayRefs.current, { xPercent: 0 });
+			tl.to(testimonialRefs.current, {
+				xPercent: 0,
+				yPercent: 0,
+				duration: 1,
+				scrollTrigger: {
+					trigger: mainContainerRef.current,
+					start: "top 15%",
+					end: "top top",
+					scrub: true,
+					markers: true,
+				},
+			}).to(overlayRefs.current, {
+				xPercent: -100,
+				stagger: 0.05,
+				scrollTrigger: {
+					trigger: mainContainerRef.current,
+					start: "top 40%",
+					end: "top 10%",
+					scrub: true,
+					markers: true,
+				},
+			});
+		}, mainContainerRef);
+	}, []);
 
-  return (
-    <div
-      ref={mainContainerRef}
-      className="h-full rounded-3xl w-full p-10 bg-neutral-400 flex flex-col gap-10"
-    >
-      <h1 className="text-6xl font-poppins font-semibold">Testimonials</h1>
-      <div className="grid grid-cols-3 gap-4 h-full w-full overflow-hidden">
-        {data.testimonials.map((item, i) => (
-          <div
-            className="flex justify-center items-center rounded-xl bg-white w-full h-full"
-            key={i}
-          >
-            <h1>{item.name}</h1>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+	return (
+		<div
+			ref={mainContainerRef}
+			className="h-full rounded-3xl w-full p-10 bg-neutral-400 flex flex-col gap-10"
+		>
+			<h1 className="text-6xl font-poppins font-semibold">Testimonials</h1>
+			<div className="grid grid-cols-3 gap-4 h-full w-full overflow-hidden">
+				{data.testimonials.map((item, i) => (
+					<div
+						ref={(el) => (testimonialRefs.current[i] = el)}
+						className="flex justify-center items-center rounded-xl bg-white w-full h-full"
+						key={i}
+					>
+						<h1>{item.name}</h1>
+					</div>
+				))}
+			</div>
+		</div>
+	);
 };
